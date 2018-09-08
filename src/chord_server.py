f29e25509e9b9b7c31ef0ba19642d029da8ace84
Aravind Kumar Dhinagaran <35083510+adhinag1@users.noreply.github.com>
2018-09-08 13:22:01 -0400

    Update chord_server.py

--- src/chord_server.py
+++ src/chord_server.py
@@ -5,7 +5,7 @@ import socket
 import sys
 
 sys.path.append('gen-py')
-sys.path.insert(0, glob.glob('/home/yaoliu/src_code/local/lib/lib/python2.7/site-packages')[0])
+sys.path.insert(0, glob.glob('/home/src_code/lib/python2.7/site-packages')[0]) # path where thrift is installed
 
 from chord import FileStore
 from chord.ttypes import NodeID, RFile, RFileMetadata, SystemException
